b49e21610617d5a1e6b60888747e09cb2b81764f
Magerskiy <48629306+Magerskiy@users.noreply.github.com>
2019-03-26 22:52:04 +0300

    Delete Protocol_Table_Code.py

--- Protocol_Table_Code.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-must = pd.read_csv('dump.csv', sep=',', header=0)
-result = must.groupby(['Source', 'Protocol'])['No.'].count()
-df = pd.DataFrame(result)
-df = df.rename(columns = {'No.': 'Number'}) 
-df.to_csv('dump2.csv')
-print(result)
